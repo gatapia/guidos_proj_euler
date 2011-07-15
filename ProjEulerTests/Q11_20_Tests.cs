923929449899ef3d6d63ef8be83773377a6b5953
Guido Tapia <guido@tapia.com.au>
2011-07-15 14:30:00 +1000

    Added problem 11 (q13)

--- ProjEulerTests/Q11_20_Tests.cs
+++ ProjEulerTests/Q11_20_Tests.cs
@@ -7,5 +7,7 @@ namespace ProjEulerTests
   {
     [Test] public void Q11() { Assert.AreEqual(1366, Q11_20.q11(String.Empty)); }  
     [Test] public void Q12() { Assert.AreEqual(648, Q11_20.q12(String.Empty)); }  
+    [Test] public void Q13() { Assert.AreEqual(70600674, Q11_20.q13(String.Empty)); }  
+    // [Test] public void Q14() { Assert.AreEqual(70600674, Q11_20.q14(String.Empty)); }  
   }
 }
