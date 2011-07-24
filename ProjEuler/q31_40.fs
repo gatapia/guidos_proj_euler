1f8ffce08e1062ee7d63bd3baa0321991d1c1aa3
Guido Tapia <guido@tapia.com.au>
2011-07-24 19:26:10 +1000

    q34 and q33

--- ProjEuler/q31_40.fs
+++ ProjEuler/q31_40.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.IO
+open System.Text
 open System.Collections
 open System.Collections.Generic
 
@@ -19,8 +20,45 @@ let q32 x =
   let isAbundant x = Utils.sumDivisors x > x  
   let upper_limit = 28123
   let allAbundants = [0..upper_limit] |> List.filter isAbundant
-  let isAbundantCached x = allAbundants |> Seq.exists (fun a -> a = x)
+  let cache = new Dictionary<int, bool>();
+  let isAbundantCached x = 
+    if cache.ContainsKey x then cache.[x]
+    else 
+      let v = allAbundants |> Seq.exists (fun a -> a = x)
+      cache.Add(x, v)
+      v
+
   let cannotBeSummedBy2Abundants x =    
-    allAbundants |> Seq.exists(fun a -> x > a && isAbundantCached (x - a))
+    not(allAbundants |> Seq.exists(fun a -> x > a && isAbundantCached(x - a)))
 
   [1..upper_limit] |> List.filter cannotBeSummedBy2Abundants |> List.sum
+
+// Q33: How many circular primes are there below one million?
+let q33 x = 
+  let getAllRotations x =
+    let orig = x.ToString()
+    let rec getAllRotationsAux (lst:list<int64>) =  
+      let len = lst.Length
+      if len = orig.Length then lst
+      else               
+        let newstr = orig.Substring(len) + orig.Substring(0, len)
+        let newint = Int64.Parse(newstr)
+        getAllRotationsAux (newint::lst)
+    getAllRotationsAux [x]
+  let rots = [2L..999999L] |> List.map getAllRotations
+  let allRotsPrime = rots |> List.filter (fun l -> l |> List.forall Utils.isPrime)
+  allRotsPrime.Length
+
+// Q34: If dn represents the nth digit of the fractional part, find the value 
+// of the following expression. 
+// d1 x d10 x d100 x d1000 x d10000 x d100000 x d1000000
+let q34 x =  
+  let sb = new StringBuilder()
+  [0..1000000] |> List.iter(fun i -> ignore(sb.Append (i)))
+  Char.GetNumericValue(sb.Chars(1)) *
+    Char.GetNumericValue(sb.Chars(10)) *
+    Char.GetNumericValue(sb.Chars(100)) *
+    Char.GetNumericValue(sb.Chars(1000)) *
+    Char.GetNumericValue(sb.Chars(10000)) *
+    Char.GetNumericValue(sb.Chars(100000)) *
+    Char.GetNumericValue(sb.Chars(1000000))
--- ProjEulerTests/Q31_40_Tests.cs
+++ ProjEulerTests/Q31_40_Tests.cs
@@ -3,12 +3,12 @@ using NUnit.Framework;
 
 namespace ProjEulerTests
 {
-  [TestFixture, Timeout(10000)] public class Q31_40_Tests
+  [TestFixture] public class Q31_40_Tests
   {
     [Test] public void Q31() { Assert.AreEqual(40730, Q31_40.q31(String.Empty)); }  
-    [Test] public void Q32() { Assert.AreEqual(0, Q31_40.q32(String.Empty)); }  
-    // [Test] public void Q33() { Assert.AreEqual(0, Q31_40.q33(String.Empty)); }  
-    // [Test] public void Q34() { Assert.AreEqual(0, Q31_40.q34(String.Empty)); }  
+    [Test] public void Q32() { Assert.AreEqual(4179871, Q31_40.q32(String.Empty)); }  
+    [Test] public void Q33() { Assert.AreEqual(55, Q31_40.q33(String.Empty)); }  
+    [Test] public void Q34() { Assert.AreEqual(210, Q31_40.q34(String.Empty)); }  
     // [Test] public void Q35() { Assert.AreEqual(0, Q31_40.q35(String.Empty)); }  
     // [Test] public void Q36() { Assert.AreEqual(0, Q31_40.q36(String.Empty)); }  
     // [Test] public void Q37() { Assert.AreEqual(0, Q31_40.q38(String.Empty)); }  
